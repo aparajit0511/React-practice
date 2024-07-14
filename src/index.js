35619e6563c792db36f950d1cb3b519bae20c6c1
aparajit0511 <aparajit35@gmail.com>
2024-07-14 13:59:20 +0530

    javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +15,33 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Hoisting
+
+console.log(x);
+getName();
+// getPrint();
+
+var x = 10;
+
+function getName() {
+  console.log("Function is called");
+}
+
+var getPrint = () => {
+  console.log("Arrow function");
+};
+
+// Closures
+
+function outerloop() {
+  var a = 7;
+
+  function innerloop() {
+    console.log(a);
+  }
+
+  innerloop();
+}
+
+outerloop();
